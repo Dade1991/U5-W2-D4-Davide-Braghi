6b3e000eb52a8909b2a32c2f610602ed525d4244
Dade1991 <dade91@msn.com>
2025-10-16 14:41:14 +0200

    Initialized PJT folder

---
+++ src/main/java/davidebraghi/U5_W2_D4_Davide_Braghi/U5W2D4DavideBraghiApplication.java
@@ -0,0 +1,13 @@
+package davidebraghi.U5_W2_D4_Davide_Braghi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W2D4DavideBraghiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W2D4DavideBraghiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davidebraghi/U5_W2_D4_Davide_Braghi/U5W2D4DavideBraghiApplicationTests.java
@@ -0,0 +1,13 @@
+package davidebraghi.U5_W2_D4_Davide_Braghi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W2D4DavideBraghiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
